273878890daaa7f6f6f74c1a2626d37910141b29
OlivetEkeh <ekehsons@gmail.com>
2023-11-28 10:06:09 -0800

    7edit

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,15 +1,3 @@
 #!/usr/bin/python3
-
-def islower(c):
-    # Get the Unicode value of the character
-    unicode_value = ord(c)
-    
-    # Check if the Unicode value is within the range of lowercase letters
-    return 97 <= unicode_value <= 122
-
-# Test the function with example cases
-print("a is {}".format("lower" if islower("a") else "upper"))
-print("H is {}".format("lower" if islower("H") else "upper"))
-print("A is {}".format("lower" if islower("A") else "upper"))
-print("3 is {}".format("lower" if islower("3") else "upper"))
-print("g is {}".format("lower" if islower("g") else "upper"))
+def islower(c)
+    return ord("a") <= ord(c) <= ord("z")
