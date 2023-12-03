e6117a6c5009914eaa588624df7750fd4c861370
OlivetEkeh <ekehsons@gmail.com>
2023-12-03 18:09:57 +0100

    edited 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,3 +1,4 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 
 void print_python_list_info(PyObject *p)
@@ -5,9 +6,15 @@ void print_python_list_info(PyObject *p)
 	Py_ssize_t size, i;
 	PyObject *item;
 
-	size = PyList_Size(p);
-	printf("[*] Size of the Python List = %ld\n", size);
+	if (!PyList_Check(p))
+	{
+		fprintf(stderr, "Invalid List Object\n");
+		return;
+	}
 
+	size = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %ld\n", size);
 	printf("[*] Allocated = %ld\n", ((PyListObject *)p)->allocated);
 
 	for (i = 0; i < size; i++)
@@ -16,3 +23,4 @@ void print_python_list_info(PyObject *p)
 		printf("Element %ld: %s\n", i, Py_TYPE(item)->tp_name);
 	}
 }
+
