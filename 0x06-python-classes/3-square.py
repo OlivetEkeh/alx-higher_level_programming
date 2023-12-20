9c7d19c6ed559f71d91e5d3bb894459f79ccc629
OlivetEkeh <ekehsons@gmail.com>
2023-12-20 02:16:46 +0100

    task1to6

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 
+"""This is the Square class. """
+
 class Square:
-    """
-    This is the Square class.
-    """
+
     def __init__(self, size=0):
         """
         This is the constructor for the Square class.
 
         Parameters:
         - size (int, optional): The size of the square. Default is 0.
+        
+        Raises:
+        TypeError: if size is not an integer
+        ValueError: if size is less than 0
+
         """
+        
         self.__size = size
 
         if not isinstance(size, int):
@@ -18,3 +24,9 @@ class Square:
 
         if size < 0:
             raise ValueError("size must be >= 0")
+
+    def area(self):
+        """area
+        The method returns the area of the square
+        """
+        return self.__size * self.__size
