41547b204915a9b7e579bf1fbb5304c9f47b0099
OlivetEkeh <ekehsons@gmail.com>
2024-01-04 19:52:13 +0100

    task 0 & 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,7 +2,10 @@
 
 """ Defines a Rectangle class """
 
+
 class Rectangle:
+    """ First implementation of the class """
+
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -16,9 +19,9 @@ class Rectangle:
     def width(self, value):
         """ set the width """
         if not isinstance(value, int):
-            raise TypeError("width must be an integer")
+            raise TypeError('width must be an integer')
         if value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError('width must be >= 0')
         self.__width = value
 
     @property
@@ -30,15 +33,7 @@ class Rectangle:
     def height(self, value):
         """ set the rectangle's height """
         if not isinstance(value, int):
-            raise TypeError("height must be an integer")
+            raise TypeError('height must be an integer')
         if value < 0:
-            raise ValueError("height must be >= 0")
+            raise ValueError('height must be >= 0')
         self.__height = value
-
-    def area(self):
-        return self.__width * self.__height
-
-    def __lt__(self, other):
-        """Define the comparison based on area """
-        return self.area() < other.area()
-
