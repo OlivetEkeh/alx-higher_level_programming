e4ade08199768953299014e33e94e7433bd64514
OlivetEkeh <ekehsons@gmail.com>
2024-01-04 20:19:43 +0100

    rectangles

---
+++ 0x08-python-more_classes/101-nqueens.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+""" A program that solves the N queens problem """
+
+import sys
+
+def is_safe(board, row, col, N):
+    """
+    Check if it's safe to place a queen at board[row][col].
+    """
+    # Check the current column
+    for i in range(row):
+        if board[i][col] == 1:
+            return False
+
+    # Check the upper diagonal
+    i, j = row, col
+    while i >= 0 and j >= 0:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j -= 1
+
+    # Check the lower diagonal
+    i, j = row, col
+    while i >= 0 and j < N:
+        if board[i][j] == 1:
+            return False
+        i -= 1
+        j += 1
+
+    return True
+
+def solve_nqueens_util(board, row, N, solutions):
+    """
+    A recursive utility function to solve the N-Queens problem.
+    """
+    if row == N:
+        # All queens have been successfully placed, add the solution
+        solution = [(i, j) for i in range(N) for j in range(N) if board[i][j] == 1]
+        solutions.append(solution)
+        return
+
+    for col in range(N):
+        if is_safe(board, row, col, N):
+            # Place the queen at board[row][col]
+            board[row][col] = 1
+
+            # Recur to place the rest of the queens
+            solve_nqueens_util(board, row + 1, N, solutions)
+
+            # Backtrack and remove the queen from board[row][col]
+            board[row][col] = 0
+
+def solve_nqueens(N):
+    """
+    Solve the N-Queens problem and print all possible solutions.
+    """
+    board = [[0 for _ in range(N)] for _ in range(N)]
+    solutions = []
+    solve_nqueens_util(board, 0, N, solutions)
+
+    for solution in solutions:
+        print(solution)
+
+if __name__ == '__main__':
+    # Get the value of N from the command line argument
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+
+    try:
+        N = int(sys.argv[1])
+        if N < 4:
+            print("N must be at least 4")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
+
+    solve_nqueens(N)
---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python3
+
+""" Defines a Rectangle class """
+
+
+class Rectangle:
+    """ First implementation of the class """
+
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """ retrieve the rectangle's width """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ set the width """
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+        self.__width = value
+
+    @property
+    def height(self):
+        """ get the rectangle's height """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ set the rectangle's height """
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+        self.__height = value
+
+    def area(self):
+        """ returns the rectangle area """
+        return self.width * self.height
+
+    def perimeter(self):
+        """ returns the rectangle perimeter """
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        else:
+            return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """ Returns the informal string representaion
+        of the rectangle """
+
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        else:
+            result = ""
+            for _ in range(self.__height):
+                result += str(self.print_symbol) * self.__width + '\n'
+            return result.rstrip()
+
+    def __repr__(self):
+        """should return a string representation of the rectangle
+        to be able to recreate a new instance by using eval() """
+
+        return f"Rectangle({self.__width}, {self.__height})"
+
+    def __del__(self):
+        """ Delete instance of class """
+        Rectangle.number_of_instances -= 1
+        print("Bye rectangle...")
+
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """
+        Returns the biggest rectangle based on the area
+        """
+
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError('rect_1 must be an instance of Rectangle')
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError('rect_2 must be an instance of Rectangle')
+
+        if rect_1.area() == rect_2.area():
+            return rect_1
+        return rect_1 if rect_1.area() > rect_2.area() else rect_2
