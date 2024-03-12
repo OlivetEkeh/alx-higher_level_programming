be20644dfb2e80c8afb45d739a2e546efa22947d
OlivetEkeh <ekehsons@gmail.com>
2024-03-12 15:34:13 +0100

    task2

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-module.exports = class Rectangles {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0 && typeof w !== 'number' && h !== 'number') {
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (typeof w === 'number' && typeof h === 'number' && w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
   }
-}
+};
