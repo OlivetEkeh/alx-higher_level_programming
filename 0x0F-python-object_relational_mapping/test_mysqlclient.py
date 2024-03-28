4b41f50571289381da917de28ba3498b08403c4f
OlivetEkeh <ekehsons@gmail.com>
2024-03-28 14:36:27 +0100

    making this thing actually usable.

---
+++ 0x0F-python-object_relational_mapping/test_mysqlclient.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+import MySQLdb
+
+# If the import statement above executes without errors,
+# it means mysqlclient is installed and accessible.
+print("mysqlclient is successfully installed.")
+
